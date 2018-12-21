b4cfbefa16dfd827c9bca1939f32726ec1b66cba
smithB <bsmith@apl.washington.edu>
2018-12-20 16:53:33 -0800

    make_rx_scat_catalog.py: function to make a catalog of waveform templates

---
+++ make_rx_scat_catalog.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 25 14:01:44 2018
+
+@author: ben
+"""
+
+import numpy as np
+import h5py
+from waveform import waveform
+
+def make_rx_scat_catalog(TX, h5_file=None):
+    """
+    make a dictionary of waveform templates by convolving the transmit pulse with 
+    subsurface-scattering SRFs
+    """
+    if h5_file is None:
+        h5_file='/Users/ben/Dropbox/ATM_red_green/subsurface_srf_no_BC.h5'
+    with h5py.File(h5_file,'r') as h5f:
+        t0=np.array(h5f['t'])
+        RX=dict()
+        for row, r_val in enumerate(h5f['r_eff']):
+            rx0=h5f['p'][row,:]            
+            temp=np.convolve(TX.p.ravel(), rx0,'same')*0.25e-9
+            RX[r_val]=waveform(TX.t, np.interp(TX.t.ravel(), t0*1.e9, temp).reshape(TX.t.shape))
+            RX[r_val].t0=0.
+            RX[r_val].tc=RX[r_val].nSigmaMean()[0]
+    return RX
+            
+            
