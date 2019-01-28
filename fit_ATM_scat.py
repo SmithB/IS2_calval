0a683948dac738abfb703dc19b8963d56dddfc3c
smithB <bsmith@apl.washington.edu>
2019-01-27 21:40:10 -0800

    fit_ATM_scat.py: made the waveform fitting an exportable function

--- fit_ATM_scat.py
+++ fit_ATM_scat.py
@@ -68,134 +68,162 @@ def get_tx_est(filename, nShots=np.Inf):
     TX.tc = np.array(TX.nSigmaMean()[0])
     TX.normalize()
     return TX
-    
 
-parser = argparse.ArgumentParser(description='Fit the waveforms from an ATM file with a set of scattering parameters')
-parser.add_argument('input_file', type=str)
-parser.add_argument('output_file', type=str)
-parser.add_argument('--startShot', '-s', type=int, default=0)
-parser.add_argument('--scat_file', '-f', type=str, default=None)
-parser.add_argument('--nShots', '-n', type=int, default=np.Inf)
-parser.add_argument('--DOPLOT', '-P', action='store_true')
-parser.add_argument('--IR', '-I', action='store_true')
-parser.add_argument('--waveforms', '-w', action='store_true', default=False)
-args=parser.parse_args()
 
-# get the waveform count from the output file 
-nWFs=np.minimum(args.nShots, read_ATM_file(args.input_file, getCountAndReturn=True)-args.startShot)
-lastShot=args.startShot+nWFs
-
-# make the output file
-if os.path.isfile(args.output_file):
-    os.remove(args.output_file)
-outDS = ['shot', 'R', 'A', 'B', 'delta_t', 'sigma', 't0', 'K0', 'Kmin', 'Kmax',\
-        'latitude', 'longitude', 'elevation']
-out_h5 = h5py.File(args.output_file,'w')
-for DS in outDS:
-    out_h5.create_dataset(DS, (nWFs,), dtype='f8')
-
-if args.waveforms:
-    out_h5.create_dataset('RX/p', (192, nWFs))
-    out_h5.create_dataset('RX/p_fit', (192, nWFs))
-
-# check whether the scattering file exists
-if args.scat_file is not None:
-    scat_file=args.scat_file
-else:
-    # look for the default scattering file in the directory where the source file is found
-    scat_file=os.path.dirname(os.path.abspath(__file__)) + 'subsurface_srf_no_BC.h5'
-if not os.path.isfile(scat_file):
-    print("%s does not exist" % scat_file)
-    exit()
- 
-# choose how to divide the output
-blocksize=1000
-start_vals=args.startShot+np.arange(0, nWFs, blocksize, dtype=int)
-
-# get the transmit pulse
-TX = get_tx_est(args.input_file, nShots=5000)
-
-# make the library of templates
-WF_library = dict()
-WF_library.update({0.:TX})
-if args.IR is False:
-    WF_library.update(make_rx_scat_catalog(TX, h5_file=scat_file))
-    R_vals=np.sort(list(WF_library))
-else:
-    R_vals=[0]
-    
-print("Returns:")
-# loop over start vals (one block at a time...)
-catalogBuffer=None
-time_old=time()
-for shot0 in start_vals:
-    D=read_ATM_file(args.input_file, shot0=shot0, nShots=np.minimum(blocksize, lastShot-shot0))
+def proc_RX(WF_file, shots, sigmas=np.arange(0, 5, 0.25), deltas=np.arange(-1, 1.5, 0.5), TX=None, countPeaks=True, WF_library=None, TX_file=None, scat_file=None, catalogBuffer=None):
+    """
+        Routine to process the wavefoms in an ATM waveform file.  Can be run inside 
+        the loop of fit_ATM_scat, or run on a few waveforms at a time for plot generation
+    """
+    if TX is None and TX_file is not None:
+        with h5py.file(TX_file) as h5f:
+            TX={'t':np.array(h5f['/TX/t']),'p':np.array(h5f(['/TX/p']))}
+     
+    # make the library of templates
+    if WF_library is None:
+        WF_library = dict()
+        WF_library.update({0.:TX}) 
+        if scat_file is not None:
+            WF_library.update(make_rx_scat_catalog(TX, h5_file=scat_file))
+     
+    D=read_ATM_file(WF_file, shot0=shots[0], nShots=shots[-1]-shots[0])
     
     # make the return waveform structure
     rxData=D['RX'][0:D['RX'].size]
     rxData.t=rxData.t-rxData.t.mean()
-    if args.IR is False:
-        nP=rxData.count_peaks(W=4)
-        sigmas=np.arange(0, 5, 0.25)
+    if countPeaks:
+        rxData.nPeaks=rxData.count_peaks(W=4)
     else:
-        nP=np.ones(rxData.size)
-        sigmas=np.arange(0, 5, 0.125)
-    rxData.nPeaks=nP
+        rxData.nPeaks=np.ones(rxData.size)
+
     threshold_mask = rxData.p >= 255
     rxData.subBG()
     rxData.tc=[ ii.nSigmaMean()[0] for ii in rxData ]
     rxData.p[threshold_mask]=np.NaN
-    
-    # choose a set of delta t values
-    deltas=np.arange(-1, 1.5, 0.5)  
-        
+             
     # fit the data. Catalogbuffer contains waveform templates that have already been tried
-    D_out, catalogBuffer=fit_catalog(rxData, WF_library, sigmas, deltas, return_data_est=args.waveforms, \
+    D_out, catalogBuffer=fit_catalog(rxData, WF_library, sigmas, deltas, return_data_est=True, \
                                      return_catalog=True, catalog=catalogBuffer)
+    return D_out, D, rxData, catalogBuffer
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Fit the waveforms from an ATM file with a set of scattering parameters')
+    parser.add_argument('input_file', type=str)
+    parser.add_argument('output_file', type=str)
+    parser.add_argument('--startShot', '-s', type=int, default=0)
+    parser.add_argument('--scat_file', '-f', type=str, default=None)
+    parser.add_argument('--nShots', '-n', type=int, default=np.Inf)
+    parser.add_argument('--DOPLOT', '-P', action='store_true')
+    parser.add_argument('--IR', '-I', action='store_true')
+    parser.add_argument('--waveforms', '-w', action='store_true', default=False)
+    args=parser.parse_args()
     
-    N_out=D_out['shot'].size
-    outShot0=shot0-args.startShot
-    for key in outDS:          
-        try:
-            if key in D_out:
-                temp=D_out[key].ravel()
-                #temp[np.isnan(temp)]=-9999
-                out_h5[key][outShot0:outShot0+N_out]=D_out[key].ravel()
-            else:
-                if key in D:
-                    out_h5[key][outShot0:outShot0+N_out]=D[key].ravel()
-        except OSError:
-            print("OSError for key=%s, outshot0=%d, outshotN=%d, nDS=%d"% (key, outShot0, outShot0+N_out, out_h5[key].size))
+    # get the waveform count from the output file 
+    nWFs=np.minimum(args.nShots, read_ATM_file(args.input_file, getCountAndReturn=True)-args.startShot)
+    lastShot=args.startShot+nWFs
+    
+    # make the output file
+    if os.path.isfile(args.output_file):
+        os.remove(args.output_file)
+    outDS = ['shot', 'R', 'A', 'B', 'delta_t', 'sigma', 't0', 'K0', 'Kmin', 'Kmax',\
+            'latitude', 'longitude', 'elevation']
+    out_h5 = h5py.File(args.output_file,'w')
+    for DS in outDS:
+        out_h5.create_dataset(DS, (nWFs,), dtype='f8')
     
     if args.waveforms:
-        out_h5['RX/p_fit'][:, outShot0:outShot0+N_out] = D_out['wf_est']
-        out_h5['RX/p'][:, outShot0:outShot0+N_out] = rxData.p
+        out_h5.create_dataset('RX/p', (192, nWFs))
+        out_h5.create_dataset('RX/p_fit', (192, nWFs))
     
-    print("  shot=%d out of %d, N_keys=%d" % (shot0+blocksize, start_vals[-1]+blocksize, len(catalogBuffer.keys())))
-    if args.DOPLOT:
-        plt.figure(); 
-        plt.subplot(511)
-        plt.plot( D_out['A'], '.')
-        plt.ylabel('amplitude')
-        plt.subplot(512)
-        plt.plot(D_out['R']/D_out['A'], '.')
-        plt.ylabel('R/A')
-        plt.subplot(513)
-        plt.plot( D_out['sigma'], '.')
-        plt.ylabel('sigma')
-        plt.subplot(514)
-        plt.plot( D_out['delta_t'], '.')
-        plt.ylabel('delta_t')
-        plt.subplot(515)
-        plt.plot( D_out['K0'], '.')
-        plt.plot( D_out['Kmax'], '.')
-        plt.ylabel('K')
-print("   time to fit RX=%3.2f" % (time()-time_old))
-
-out_h5.create_dataset("TX/t", data=TX.t.ravel())
-out_h5.create_dataset("TX/p", data=TX.p.ravel())
-if args.waveforms:
-    out_h5.create_dataset('RX/t', data=rxData.t.ravel())
+    # check whether the scattering file exists
+    if args.scat_file is not None:
+        scat_file=args.scat_file
+    else:
+        # look for the default scattering file in the directory where the source file is found
+        scat_file=os.path.dirname(os.path.abspath(__file__)) + 'subsurface_srf_no_BC.h5'
+    if not os.path.isfile(scat_file):
+        print("%s does not exist" % scat_file)
+        exit()
+     
+    # choose how to divide the output
+    blocksize=1000
+    start_vals=args.startShot+np.arange(0, nWFs, blocksize, dtype=int)
     
-out_h5.close() 
+    # get the transmit pulse
+    TX = get_tx_est(args.input_file, nShots=5000)
+    
+    # make the library of templates
+    WF_library = dict()
+    WF_library.update({0.:TX})
+    if args.IR is False:
+        WF_library.update(make_rx_scat_catalog(TX, h5_file=scat_file))
+        R_vals=np.sort(list(WF_library))
+    else:
+        R_vals=[0]
         
+    print("Returns:")
+    # loop over start vals (one block at a time...)
+    catalogBuffer=None
+    time_old=time()
+    
+    if args.IR is True:
+        countPeaks=False
+        sigmas=np.arange(0, 5, 0.125)
+    else:
+        countPeaks=True   
+        sigmas=np.arange(0, 5, 0.25)
+    # choose a set of delta t values
+        deltas=np.arange(-1, 1.5, 0.5)  
+    
+    for shot0 in start_vals:
+        shots=np.arange(shot0, np.minimum(shot0+blocksize, lastShot))
+        D_out, D, rxData, catalogBuffer=proc_RX(args.input_file, shots, sigmas=sigmas, deltas=deltas,\
+             TX=TX, WF_library=WF_library, catalogBuffer=catalogBuffer, countPeaks=countPeaks)
+             
+        N_out=D_out['shot'].size
+        outShot0=shot0-args.startShot
+        for key in outDS:          
+            try:
+                if key in D_out:
+                    out_h5[key][outShot0:outShot0+N_out]=D_out[key].ravel()
+                else:
+                    if key in D:
+                        out_h5[key][outShot0:outShot0+N_out]=D[key].ravel()
+            except OSError:
+                print("OSError for key=%s, outshot0=%d, outshotN=%d, nDS=%d"% (key, outShot0, outShot0+N_out, out_h5[key].size))
+        
+        if args.waveforms:
+            out_h5['RX/p_fit'][:, outShot0:outShot0+N_out] = D_out['wf_est']
+            out_h5['RX/p'][:, outShot0:outShot0+N_out] = rxData.p
+        
+        print("  shot=%d out of %d, N_keys=%d" % (shot0+blocksize, start_vals[-1]+blocksize, len(catalogBuffer.keys())))
+        if args.DOPLOT:
+            plt.figure(); 
+            plt.subplot(511)
+            plt.plot( D_out['A'], '.')
+            plt.ylabel('amplitude')
+            plt.subplot(512)
+            plt.plot(D_out['R']/D_out['A'], '.')
+            plt.ylabel('R/A')
+            plt.subplot(513)
+            plt.plot( D_out['sigma'], '.')
+            plt.ylabel('sigma')
+            plt.subplot(514)
+            plt.plot( D_out['delta_t'], '.')
+            plt.ylabel('delta_t')
+            plt.subplot(515)
+            plt.plot( D_out['K0'], '.')
+            plt.plot( D_out['Kmax'], '.')
+            plt.ylabel('K')
+    print("   time to fit RX=%3.2f" % (time()-time_old))
+    
+    out_h5.create_dataset("TX/t", data=TX.t.ravel())
+    out_h5.create_dataset("TX/p", data=TX.p.ravel())
+    if args.waveforms:
+        out_h5.create_dataset('RX/t', data=rxData.t.ravel())
+        
+    out_h5.close() 
+            
+if __name__=="__main__":
+    main()
