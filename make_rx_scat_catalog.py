443a11964b71890da18a33062d37d91edb6a49aa
Ben Smith <bsmith@apl.washington.edu>
2019-01-30 11:05:33 -0700

    make_rx_scat_catalog.py: added location for waveform import

--- make_rx_scat_catalog.py
+++ make_rx_scat_catalog.py
@@ -8,7 +8,7 @@ Created on Sun Nov 25 14:01:44 2018
 
 import numpy as np
 import h5py
-from waveform import waveform
+from IS2_calval.waveform import waveform
 
 def make_rx_scat_catalog(TX, h5_file=None):
     """
